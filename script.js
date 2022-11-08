5497f44802dd24275e4b99ce5a336bdebbbbf624
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-07 22:29:39 -0700

    created the mesh, scene, camera and renderer

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log(THREE);
+const canvas = document.querySelector('.webgl');
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Scene contains actors, cameras, etc
+const scene = new THREE.Scene();
+
+// Creating cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// Creating material (using recommended hexadecimal color)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+// Creating mesh and adding it to the scene
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); // Field of view (vision angle), aspect ratio
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
